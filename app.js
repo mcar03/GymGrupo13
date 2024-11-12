52ed49b68db5e9fbf75d73a6f2f9b91ae8e2c93e
JuanraDAM <juanrafaelfered@gmail.com>
2024-11-12 09:46:08 +0100

    faltan los controllers

--- app.js
+++ app.js
@@ -1,68 +1,56 @@
+/**
+ * Aplicación full-stack gestión académica.
+ */
+
 const express = require('express');
 const session = require('express-session');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-require('dotenv').config({ path: './gym/.env' }); // Relative Path: gym\.env
+const clasesRouter = require('./routes/clasesRouter');
+const clientesRouter = require('./routes/clientesRouter');
+const entrenadorRouter = require('./routes/entrenadorRouter')
+const membresiasRouter = require('./routes/membresiasRouter')
+const inscripcionesMembresiasRouter = require('./routes/inscripcionesMembresiasRouter')
+
+require('dotenv').config({ path: './gesaca/.env' });
 
 /**
  * Crea el servidor Web
  */
-
 const app = express();
 const port = process.env.SERVICE_PORT;
-
-
+;
 /**
  * Configuramos el motor de plantillas 
+ * 
  */
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /**
- * Middleware para llevar la gestión de sesiones.
- * Si no hay sesión iniciada, arrancamos...
+ * Delegamos todas las rutas que comienzan por alumnos, asignaturas, etc.
+ * al enrutador correspondiente
  */
-
-app.use(session({
-    secret: 'misupersecretoquenadiesabe',
-    resave: true,
-    saveUninitialized: false
-}));
-
-
-// cargarmos y configuramos el middleware para gestión de sesiones
-app.use((req,res,next)=>{
-    res.locals.currentUser = req.session.user;
-    if (!req.session.user){        
-        if (req.path.startsWith('/auth/login') ||
-            req.path.startsWith('/auth/register')){
-            // para hacer el GET/POST al login
-            next();            
-        } else {
-            // cuando es una ruta distinta a login
-            // me redirecciona al login
-            return res.redirect('/auth/login');
-        }
-    } else {
-        // ya estamos logeados        
-        next();
-    }
-});
-
-app.use('/auth', authRouter);
-app.use('/alumnos', alumnoRouter);
-app.use('/asignaturas', asignaturaRouter);
-
+app.use('/clases', clasesRouter);
+app.use('/clientes', clientesRouter);
+app.use('/entrenador', entrenadorRouter);
+app.use('/membresias', membresiasRouter);
+app.use('/inscripciones', inscripcionesMembresiasRouter);
 
 app.get('/', (req, res) => {
-    if (req.session.user)
-        res.render('index', {user: req.session.user, titulo: 'Inicio'})
-    else 
-        res.redirect('/login')
+    res.render('index')
 });
 
 
+
+/**
+ * Siempre lo último que hacemos
+ */
+app.listen(
+    port, () => {
+        console.log(`Servidor iniciado en http://localhost:${port}`);
+    });
 
