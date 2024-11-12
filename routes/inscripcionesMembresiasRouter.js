05d24ee11433b139e8f0e852e201e5acdeb011d1
JuanraDAM <juanrafaelfered@gmail.com>
2024-11-12 09:58:52 +0100

    parte juanra (test V1.0)

--- routes/inscripcionesMembresiasRouter.js
+++ routes/inscripcionesMembresiasRouter.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const membresiaController = require('../controllers/membresiaController');
 const inscripcionController = require('../controllers/inscripcionMembresiaController');
 
 
 // Rutas para manejar Inscripciones a Membresías
 // Ruta para ver todas las inscripciones de los clientes a las membresías
-router.get('/inscripciones', inscripcionController.inscripciones);
+router.get('/', inscripcionController.inscripciones);
 
 // Ruta para mostrar el formulario de agregar una nueva inscripción a una membresía
-router.get('/inscripciones/add', inscripcionController.inscripcionAddFormulario);
+router.get('/add', inscripcionController.inscripcionAddFormulario);
 
 // Ruta para manejar la creación de una nueva inscripción a una membresía
-router.post('/inscripciones/add', inscripcionController.inscripcionAdd);
+router.post('/add', inscripcionController.inscripcionAdd);
 
 // Ruta para eliminar una inscripción (por id)
-router.get('/inscripciones/del/:id', inscripcionController.inscripcionDelFormulario);
+router.get('/del/:id', inscripcionController.inscripcionDelFormulario);
 
 // Ruta para manejar la eliminación de una inscripción (por id)
-router.post('/inscripciones/del/:id', inscripcionController.inscripcionDel);
+router.post('/del/:id', inscripcionController.inscripcionDel);
 
 // Ruta para editar una inscripción (por id)
-router.get('/inscripciones/edit/:id', inscripcionController.inscripcionEditFormulario);
+router.get('/edit/:id', inscripcionController.inscripcionEditFormulario);
 
 // Ruta para manejar la edición de una inscripción (por id)
-router.post('/inscripciones/edit/:id', inscripcionController.inscripcionEdit);
+router.post('/edit/:id', inscripcionController.inscripcionEdit);
 
 module.exports = router;
